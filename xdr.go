f77585c4ed9c27a2d05e8b4d417943d052496ef2
Abhijit Hoskeri <abhijithoskeri@gmail.com>
2016-04-02 00:23:08 -0700

    encode integers

---
+++ xdr.go
@@ -0,0 +1,44 @@
+package sunrpc
+
+// XDR encoding
+// implements: /usr/share/doc/RFC/standard/rfc4506.txt.gz
+
+import "reflect"
+import "encoding/binary"
+import "math"
+
+func Marshal(v interface{}, data []byte) error {
+	var pos = 0
+	var end = 0
+	var val = reflect.ValueOf(v)
+	switch val.Kind() {
+	case reflect.Uint8, reflect.Uint16, reflect.Uint32:
+		end = pos + 4
+		binary.BigEndian.PutUint32(data[pos:end], uint32(val.Uint()))
+		fallthrough
+	case reflect.Int8, reflect.Int16, reflect.Int32:
+		end = pos + 4
+		binary.BigEndian.PutUint32(data[pos:end], uint32(val.Uint()))
+		fallthrough
+	case reflect.Int64:
+		end = pos + 8
+		binary.BigEndian.PutUint64(data[pos:end], val.Uint())
+		fallthrough
+	case reflect.Uint64:
+		end = pos + 8
+		binary.BigEndian.PutUint64(data[pos:end], val.Uint())
+		fallthrough
+	case reflect.Float32:
+		end = pos + 4
+		binary.BigEndian.PutUint32(data[pos:end], math.Float32bits(float32(val.Float())))
+		fallthrough
+	case reflect.Float64:
+		end = pos + 8
+		binary.BigEndian.PutUint64(data[pos:end], math.Float64bits(val.Float()))
+		fallthrough
+	default:
+		pos = end
+		break
+	}
+	return nil
+}
