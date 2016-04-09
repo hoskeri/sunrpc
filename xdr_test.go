d8ee350c08e935d592bbeefd885726c39b9ccc2f
Abhijit Hoskeri <abhijithoskeri@gmail.com>
2016-04-08 21:52:21 -0700

    int encoding test

--- xdr_test.go
+++ xdr_test.go
@@ -1,9 +1,19 @@
 package sunrpc
 
 import "testing"
+import "encoding/hex"
+
+func Assert(t *testing.T, tag string, got []byte, exp string) {
+	if hex.EncodeToString(got) != exp {
+		t.Error(tag, exp, got)
+	}
+}
 
 func TestMarshal(t *testing.T) {
-
+	var enc uint32 = 255
+	var xdr = make([]byte, 4)
+	Marshal(enc, xdr)
+	Assert(t, "uint32", xdr, "000000ff")
 }
 
 func TestUnmarshal(t *testing.T) {
